82490149d17f088a8e34019cfd6e7996eaeab0d2
xuanyee <72719831+xuanyee@users.noreply.github.com>
2021-04-10 10:30:30 +0800

    merged exit popup
    merged exit popup with main

--- Outback/Outback/Source/GameOverScreen.cpp
+++ Outback/Outback/Source/GameOverScreen.cpp
@@ -21,6 +21,14 @@ f32 currGG = -0.39f;
 int selection = 0;
 char gameOverMsg[100], brestart[100], mainMenu[100], quit[100], barrow[100], bcomment[100];
 
+char ggQuit[100], ggConfirm[100];
+int selectedA;
+AEGfxVertexList* ggExitMesh;
+AEGfxTexture* ggExitTex;
+AEGfxVertexList* selectionMesh;
+int selectX;
+int a = 0;		//int to prevent falling thru issue
+
 
 void OBGameOverLoad()
 {
@@ -48,75 +56,163 @@ void OBGameOverInit()
 
 	memset(bcomment, 0, 100 * sizeof(char));
 	sprintf_s(bcomment, "Left/Right to control, SPACE to choose");
+
+	AEGfxMeshStart();
+	AEGfxTriAdd(
+		-100.0f * 2, -100.0f, 0x00FF00FF, 0.0f, 0.0f,
+		100.0f * 2, -100.0f, 0x00FFFF00, 0.0f, 0.0f,
+		-100.0f * 2, 100.0f, 0x0000FFFF, 0.0f, 0.0f);
+	AEGfxTriAdd(
+		100.0f * 2, -100.0f, 0x00FFFFFF, 0.0f, 0.0f,
+		-100.0f * 2, 100.0f, 0x00FFFFFF, 0.0f, 0.0f,
+		100.0f * 2, 100.0f, 0x00FFFFFF, 0.0f, 0.0f);
+
+	ggExitMesh = AEGfxMeshEnd();
+	AE_ASSERT_MESG(ggExitMesh, "Failed to create range Mesh!");
+	ggExitTex = AEGfxTextureLoad("..\\Resources\\Textures\\pausepop.png");
+	AE_ASSERT_MESG(ggExitTex, "Failed to create pause text!!");
+	memset(ggQuit, 0, 100 * sizeof(char));
+	sprintf_s(ggQuit, "Please confirm to quit the program");
+	memset(ggConfirm, 0, 100 * sizeof(char));
+	sprintf_s(ggConfirm, "YES / NO");
+	selectedA = 0;
+
+	AEGfxMeshStart();
+	AEGfxTriAdd(
+		-25.0f, -10.5f, 0xFFFF0000, 0.0f, 1.0f,
+		25.0f, -10.5f, 0xFFFF0000, 1.0f, 1.0f,
+		-25.0f, 10.5f, 0xFFFF0000, 0.0f, 0.0f);
+	AEGfxTriAdd(
+		25.0, -10.5f, 0xFFFF0000, 1.0f, 1.0f,
+		25.0f, 10.5f, 0xFFFF0000, 1.0f, 0.0f,
+		-25.0f, 10.5f, 0xFFFF0000, 0.0f, 0.0f);
+	selectionMesh = AEGfxMeshEnd();
+	AE_ASSERT_MESG(selectionMesh, "Failed to create Enemy Mesh!");
+	selectX = -17;
+
 }
 
 void OBGameOverUpdate()
 {
-	if (AEInputCheckTriggered(AEVK_RIGHT) || AEInputCheckTriggered(AEVK_D))
+	if (selectedA == 0)
 	{
-		currGG += 0.30f;
-		selection += 1;
-	}
+		if (AEInputCheckTriggered(AEVK_RIGHT) || AEInputCheckTriggered(AEVK_D))
+		{
+			currGG += 0.30f;
+			selection += 1;
+		}
 
-	if (AEInputCheckTriggered(AEVK_LEFT) || AEInputCheckTriggered(AEVK_A))
+		if (AEInputCheckTriggered(AEVK_LEFT) || AEInputCheckTriggered(AEVK_A))
+		{
+			currGG -= 0.30f;
+			selection -= 1;
+		}
+
+		if (AEInputCheckTriggered(AEVK_SPACE) && selection == 2)
+		{
+			gGameStateNext = GS_LEVEL1;
+			level = 0;
+		}
+
+		//if (AEInputCheckTriggered(AEVK_RETURN) && currPause == 0.15f)
+			//gGameStateNext = OB_INSTRUCTIONS;
+
+		//if (AEInputCheckTriggered(AEVK_RETURN) && currGG == 0.00f)
+			//gGameStateNext = OB_SETTINGS;
+
+		//if (AEInputCheckTriggered(AEVK_RETURN) && currGG == -0.15f)
+			//gGameStateNext = OB_CREDITS;
+
+		if (AEInputCheckTriggered(AEVK_SPACE) && selection == 1)
+			gGameStateNext = GS_MAINMENU;
+
+		if (AEInputCheckTriggered(AEVK_SPACE) && selection == 0)
+			selectedA = 1;
+
+		if (AEInputCheckTriggered(AEVK_ESCAPE))
+			gGameStateNext = GS_MAINMENU;
+
+		if (selection > 2)
+		{
+			currGG = -0.39f;
+			selection = 0;
+		}
+		if (selection < 0)
+		{
+			currGG = 0.21f;
+			selection = 2;
+		}
+	}
+	if (selectedA == 1)
 	{
-		currGG -= 0.30f;
-		selection -= 1;
+		if (AEInputCheckTriggered(AEVK_A) || AEInputCheckTriggered(AEVK_LEFT))
+		{
+			if (a != 1)
+				a = 1;
+			selectX -= 62;
+		}
+		if (AEInputCheckTriggered(AEVK_D) || AEInputCheckTriggered(AEVK_RIGHT))
+		{
+			if (a != 1)
+				a = 1;
+			selectX += 62;
+		}
+		if (selectX < -17)
+			selectX = 45;
+		if (selectX > 45)
+			selectX = -17;
+		if (AEInputCheckTriggered(AEVK_SPACE) && a == 1)
+		{
+			if (selectX == -17)
+			{
+				gGameStateNext = GS_QUIT;
+			}
+			else
+			{
+				selectedA = 0;
+			}
+		}
 	}
-
-	if (AEInputCheckTriggered(AEVK_SPACE) && selection == 2)
-	{
-		gGameStateNext = GS_LEVEL1;
-		level = 0;
-	}
-
-	//if (AEInputCheckTriggered(AEVK_RETURN) && currPause == 0.15f)
-		//gGameStateNext = OB_INSTRUCTIONS;
-
-	//if (AEInputCheckTriggered(AEVK_RETURN) && currGG == 0.00f)
-		//gGameStateNext = OB_SETTINGS;
-
-	//if (AEInputCheckTriggered(AEVK_RETURN) && currGG == -0.15f)
-		//gGameStateNext = OB_CREDITS;
-
-	if (AEInputCheckTriggered(AEVK_SPACE) && selection == 1)
-		gGameStateNext = GS_MAINMENU;
-
-	if (AEInputCheckTriggered(AEVK_SPACE) && selection == 0)
-		gGameStateNext = GS_QUIT;
-
-	if (AEInputCheckTriggered(AEVK_ESCAPE))
-		gGameStateNext = GS_MAINMENU;
-
-	if (selection > 2)
-	{
-		currGG = -0.39f;
-		selection = 0;
-	}
-	if (selection < 0)
-	{
-		currGG = 0.21f;
-		selection = 2;
-	}
-
 }
 
 void OBGameOverDraw()
 {
-	//char strBuffer[100];
-	//memset(strBuffer, 0, 100 * sizeof(char));
-	//sprintf_s(strBuffer, "OUTBACK");
-	AEGfxSetRenderMode(AE_GFX_RM_COLOR);
-	AEGfxSetBlendMode(AE_GFX_BM_BLEND);
-	AEGfxTextureSet(NULL, 0, 0);
-	AEGfxSetTransparency(1.0f);
-	AEGfxPrint(fontID, gameOverMsg, -0.5, 0.50f, 1.1f, 1.0f, 1.0f, 1.0f);
-	AEGfxPrint(fontID, brestart, 0.2, 0.15f, 1.0f, 1.0f, 1.0f, 1.0f);
-	AEGfxPrint(fontID, mainMenu, -0.16f, 0.15f, 1.0f, 1.0f, 1.0f, 1.0f);
-	AEGfxPrint(fontID, quit, -0.4f, 0.15f, 1.0f, 1.0f, 1.0f, 1.0f);
-	AEGfxPrint(fontID, barrow, currGG, 0.07f, 1.0f, 1.0f, 1.0f, 1.0f);
-	AEGfxPrint(fontID, bcomment, -0.45f, -0.20f, 1.0f, 1.0f, 1.0f, 1.0f);
-	
+	if (selectedA == 0)
+	{
+		AEGfxSetRenderMode(AE_GFX_RM_COLOR);
+		AEGfxSetBlendMode(AE_GFX_BM_BLEND);
+		AEGfxTextureSet(NULL, 0, 0);
+		AEGfxSetTransparency(1.0f);
+		AEGfxPrint(fontID, gameOverMsg, -0.5, 0.50f, 1.1f, 1.0f, 1.0f, 1.0f);
+		AEGfxPrint(fontID, brestart, 0.2, 0.15f, 1.0f, 1.0f, 1.0f, 1.0f);
+		AEGfxPrint(fontID, mainMenu, -0.16f, 0.15f, 1.0f, 1.0f, 1.0f, 1.0f);
+		AEGfxPrint(fontID, quit, -0.4f, 0.15f, 1.0f, 1.0f, 1.0f, 1.0f);
+		AEGfxPrint(fontID, barrow, currGG, 0.07f, 1.0f, 1.0f, 1.0f, 1.0f);
+		AEGfxPrint(fontID, bcomment, -0.45f, -0.20f, 1.0f, 1.0f, 1.0f, 1.0f);
+	}
+	if (selectedA == 1)
+	{
+		AEGfxSetRenderMode(AE_GFX_RM_TEXTURE);
+		// Set position for object 2
+		AEGfxSetPosition(0.0f, -0.1f);	//rtriangle
+		// No tint
+		AEGfxSetTintColor(1.0f, 1.0f, 1.0f, 1.0f);
+		// Set texture
+		AEGfxTextureSet(ggExitTex, 1, 1);
+		AEGfxMeshDraw(ggExitMesh, AE_GFX_MDM_TRIANGLES);
+
+		if (a == 1)
+		{
+			AEGfxSetRenderMode(AE_GFX_RM_COLOR);
+			AEGfxMeshDraw(selectionMesh, AE_GFX_MDM_TRIANGLES);
+			AEGfxSetPosition(selectX, -52);
+		}
+		AEGfxPrint(fontID, ggQuit, -0.41f, 0.16f, 1.0f, 1.0f, 1.0f, 1.0f);
+		AEGfxPrint(fontID, bcomment, -0.45f, -0.06f, 1.0f, 1.0f, 1.0f, 1.0f);
+		AEGfxPrint(fontID, ggConfirm, -0.1f, -0.26f, 1.0f, 1.0f, 1.0f, 1.0f);
+
+
+	}
 }
 
 void OBGameOverFree()
--- Outback/Outback/Source/MainMenu.cpp
+++ Outback/Outback/Source/MainMenu.cpp
@@ -26,6 +26,17 @@ bool fullscreen;
 AEGfxVertexList *vertex = 0, *vertexBG = 0;
 AEGfxTexture	*texture = 0;
 
+char MainQuit[100], MainConfirm[100], exitComment[100];
+AEGfxVertexList* MainExitMesh;
+AEGfxTexture* MainExitTex;
+AEGfxVertexList* selectionMainMesh;
+int selectMainX;
+int b = 0;
+extern					s8 fontID;
+int						exitDia;
+
+
+
 /******************************************************************************/
 /*!
 	"Load" Main menu
@@ -70,6 +81,42 @@ void GameStateMainMenuLoad()
 	vertex = AEGfxMeshEnd();
 	AE_ASSERT_MESG(vertex, "Failed to create Enemy Mesh!");
 
+
+	AEGfxMeshStart();
+	AEGfxTriAdd(
+		-100.0f * 2, -100.0f, 0x00FF00FF, 0.0f, 0.0f,
+		100.0f * 2, -100.0f, 0x00FFFF00, 0.0f, 0.0f,
+		-100.0f * 2, 100.0f, 0x0000FFFF, 0.0f, 0.0f);
+	AEGfxTriAdd(
+		100.0f * 2, -100.0f, 0x00FFFFFF, 0.0f, 0.0f,
+		-100.0f * 2, 100.0f, 0x00FFFFFF, 0.0f, 0.0f,
+		100.0f * 2, 100.0f, 0x00FFFFFF, 0.0f, 0.0f);
+
+	MainExitMesh = AEGfxMeshEnd();
+	AE_ASSERT_MESG(MainExitMesh, "Failed to create range Mesh!");
+	MainExitTex = AEGfxTextureLoad("..\\Resources\\Textures\\pausepop.png");
+	AE_ASSERT_MESG(MainExitTex, "Failed to create pause text!!");
+	memset(MainQuit, 0, 100 * sizeof(char));
+	sprintf_s(MainQuit, "Please confirm to quit the program");
+	memset(MainConfirm, 0, 100 * sizeof(char));
+	sprintf_s(MainConfirm, "YES / NO");
+	memset(exitComment, 0, 100 * sizeof(char));
+	sprintf_s(exitComment, "Left/Right to control, SPACE to choose");
+
+	//selection mesh
+	AEGfxMeshStart();
+	AEGfxTriAdd(
+		-25.0f, -10.5f, 0xFFFF0000, 0.0f, 1.0f,
+		25.0f, -10.5f, 0xFFFF0000, 1.0f, 1.0f,
+		-25.0f, 10.5f, 0xFFFF0000, 0.0f, 0.0f);
+	AEGfxTriAdd(
+		25.0, -10.5f, 0xFFFF0000, 1.0f, 1.0f,
+		25.0f, 10.5f, 0xFFFF0000, 1.0f, 0.0f,
+		-25.0f, 10.5f, 0xFFFF0000, 0.0f, 0.0f);
+	selectionMainMesh = AEGfxMeshEnd();
+	AE_ASSERT_MESG(selectionMainMesh, "Failed to create Enemy Mesh!");
+	selectMainX = -17;
+
 	MainMenuBGMLoad();
 }
 
@@ -104,71 +151,102 @@ void GameStateMainMenuUpdate()
 	4. if down key
 		cursor -= 0.15f
 	*/
-
-	if (curr > 3)
-		curr = 0;
-
-	if (curr < 0)
-		curr = 3;
-
-	if (curMainMenu > 0.15f * 360)
-		curMainMenu = -0.3f * 270;
-
-	if (curMainMenu < -0.3f * 360)
-		curMainMenu = 0.15f * 360;
-
-	if (AEInputCheckTriggered(AEVK_UP) || AEInputCheckTriggered(AEVK_W))
+	if (exitDia == 0)
 	{
-		curMainMenu += 0.15f * 300;
-		curr--;
-	}
+		if (curr > 3)
+			curr = 0;
 
-	if (AEInputCheckTriggered(AEVK_DOWN) || AEInputCheckTriggered(AEVK_S))
-	{
-		curMainMenu -= 0.15f * 300;
-		curr++;
-	}
+		if (curr < 0)
+			curr = 3;
 
-	if (AEInputCheckReleased(AEVK_RETURN) && curr == 0)
-	{
-		gGameStateNext = GS_LVLSELECT;
-	}
+		if (curMainMenu > 0.15f * 360)
+			curMainMenu = -0.3f * 270;
 
-	if (AEInputCheckReleased(AEVK_RETURN) && curr == 1)
-	{
-		gGameStateNext = GS_LEVEL2;
-	}
+		if (curMainMenu < -0.3f * 360)
+			curMainMenu = 0.15f * 360;
 
-	if (AEInputCheckReleased(AEVK_RETURN) && curr == 2)
-	{
-		gGameStateNext = GS_CREDITS;
-	}
-
-	if (AEInputCheckReleased(AEVK_RETURN) && curr == 3
-		|| AEInputCheckTriggered(AEVK_ESCAPE))
-	{
-		gGameStateNext = GS_QUIT;
-	}
-
-	if (AEInputCheckTriggered(AEVK_N))
-	{
-		if (fullscreen == true)
+		if (AEInputCheckTriggered(AEVK_UP) || AEInputCheckTriggered(AEVK_W))
 		{
-			fullscreen = false;
-		}
-		else
-		{
-			fullscreen = true;
+			curMainMenu += 0.15f * 300;
+			curr--;
 		}
 
-		AEToogleFullScreen(fullscreen);
-		if (endless == true)
+		if (AEInputCheckTriggered(AEVK_DOWN) || AEInputCheckTriggered(AEVK_S))
 		{
-			endless = false;
+			curMainMenu -= 0.15f * 300;
+			curr++;
 		}
-		else if (endless == false)
+
+		if (AEInputCheckReleased(AEVK_RETURN) && curr == 0)
 		{
-			endless = true;
+			gGameStateNext = GS_LVLSELECT;
+		}
+
+		if (AEInputCheckReleased(AEVK_RETURN) && curr == 1)
+		{
+			gGameStateNext = GS_LEVEL2;
+		}
+
+		if (AEInputCheckReleased(AEVK_RETURN) && curr == 2)
+		{
+			gGameStateNext = GS_CREDITS;
+		}
+
+		if (AEInputCheckReleased(AEVK_RETURN) && curr == 3)
+		{
+			exitDia = 1;
+		}
+
+		if (AEInputCheckTriggered(AEVK_N))
+		{
+			if (fullscreen == true)
+			{
+				fullscreen = false;
+			}
+			else
+			{
+				fullscreen = true;
+			}
+
+			AEToogleFullScreen(fullscreen);
+			if (endless == true)
+			{
+				endless = false;
+			}
+			else if (endless == false)
+			{
+				endless = true;
+			}
+		}
+	}
+	if (exitDia == 1)
+	{
+		if (AEInputCheckTriggered(AEVK_A) || AEInputCheckTriggered(AEVK_LEFT))
+		{
+			if (b != 1)
+				b = 1;
+			selectMainX -= 62;
+		}
+		if (AEInputCheckTriggered(AEVK_D) || AEInputCheckTriggered(AEVK_RIGHT))
+		{
+			if (b != 1)
+				b = 1;
+			selectMainX += 62;
+		}
+		if (selectMainX < -17)
+			selectMainX = 45;
+		if (selectMainX > 45)
+			selectMainX = -17;
+		if (AEInputCheckTriggered(AEVK_SPACE) && b == 1)
+		{
+			if (selectMainX == -17)
+			{
+				gGameStateNext = GS_QUIT;
+			}
+			else
+			{
+				exitDia = 0;
+			}
 		}
 	}
 }
@@ -194,12 +272,14 @@ void GameStateMainMenuDraw()
 	AEGfxSetBlendMode(AE_GFX_BM_BLEND);
 	AEGfxSetTransparency(1.0f);
 
-	//For the highlight
-	AEGfxSetRenderMode(AE_GFX_RM_COLOR);
-	AEGfxMeshDraw(vertex, AE_GFX_MDM_TRIANGLES);
-	// Set position for the highlight
-	AEGfxSetPosition(-230, curMainMenu);
-
+	if (exitDia == 0)
+	{
+		//For the highlight
+		AEGfxSetRenderMode(AE_GFX_RM_COLOR);
+		AEGfxMeshDraw(vertex, AE_GFX_MDM_TRIANGLES);
+		// Set position for the highlight
+		AEGfxSetPosition(-230, curMainMenu);
+	}
 	//The title's position
 	AEGfxPrint(fontTitle, strBuffer, -0.8f, 0.60f, 1.0f, 1.0f, 1.0f, 1.0f);
 	//AEGfxPrint(fontID, "Please press 'ENTER' to confirm selection", -0.65f, 0.30f, 1.0f, 1.0f, 1.0f, 1.0f);
@@ -213,7 +293,28 @@ void GameStateMainMenuDraw()
 	//AEGfxPrint(fontID, "Press 'BACKSPACE' to return to Main Menu", -0.65f, -0.45f, 1.0f, 1.0f, 1.0f, 1.0f);
 	//AEGfxPrint(fontID, "Collect all the coins to win!", -0.45f, -0.75f, 1.0f, 1.0f, 1.0f, 1.0f);
 	//AEGfxPrint(fontID, "WASD and arrow keys compatable!", -0.55f, -0.90f, 1.0f, 1.0f, 1.0f, 1.0f);
-	
+	if (exitDia == 1)
+	{
+		AEGfxSetRenderMode(AE_GFX_RM_TEXTURE);
+		// Set position for object 2
+		AEGfxSetPosition(0.0f, 0.0f);	//rtriangle
+		// No tint
+		AEGfxSetTintColor(1.0f, 1.0f, 1.0f, 1.0f);
+		// Set texture
+		AEGfxTextureSet(MainExitTex, 1, 1);
+		AEGfxMeshDraw(MainExitMesh, AE_GFX_MDM_TRIANGLES);
+
+		if (b == 1)
+		{
+			AEGfxSetRenderMode(AE_GFX_RM_COLOR);
+			AEGfxMeshDraw(selectionMainMesh, AE_GFX_MDM_TRIANGLES);
+			// Set position for the highlight
+			AEGfxSetPosition(selectMainX, -52);
+		}
+		AEGfxPrint(fontID, MainQuit, -0.41f, 0.20f, 1.0f, 1.0f, 1.0f, 1.0f);
+		AEGfxPrint(fontID, exitComment, -0.45f, 0.0f, 1.0f, 1.0f, 1.0f, 1.0f);
+		AEGfxPrint(fontID, MainConfirm, -0.1f, -0.2f, 1.0f, 1.0f, 1.0f, 1.0f);
+	}
 
 
 }
